425e58bd31566eaee6d222c047c17c23e72b1081
Erik Slowikowski <eslowikowski@bigpoint.net>
2016-10-18 23:00:58 +0200

    Added EnemyController

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    private Rigidbody _enemyRB;
+    public float MoveSpeed;
+
+    public PlayerController ThePlayer;
+
+	// Use this for initialization
+	void Start () {
+        _enemyRB = GetComponent<Rigidbody>();
+        ThePlayer = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(ThePlayer.transform.position);
+        _enemyRB.velocity = transform.forward * MoveSpeed;
+    }
+}
